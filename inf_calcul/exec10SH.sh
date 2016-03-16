a9f3505b1620c2ae1b2ae55b0db9d5b84821c0fe
Nobuaki Fuji <seismobassoon@gmail.com>
2016-03-16 11:54:09 +0100

    added

--- inf_calcul/exec10SH.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpirun -np 5 /home/dtp02/fuji/DSM-KS2011//programs/SGTsh-0.1-MPI/mpiSGTsh < /home/dtp02/fuji/DSM-KS2011//inf_calcul//input10.inf > /home/dtp02/fuji/DSM-KS2011//log/10.ak135.logSH
--- inf_calcul/exec600SH.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpirun -np 16 /home/dtp/dtp02/fuji/DSM-KS2011/bin/mpiSGTsh < /home/dtp/dtp02/fuji/DSM-KS2011//inf_calcul//input600.inf > /home/dtp/dtp02/fuji/DSM-KS2011//log/600.ak135.logSH
