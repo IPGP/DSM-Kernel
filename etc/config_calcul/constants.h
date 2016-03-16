e31c8822496faa2b6edcb3d280bd7046ad0a57e4
Nobuaki Fuji <seismobassoon@gmail.com>
2016-03-16 18:36:44 +0100

    putain de underscore!

---
+++ etc/config_calcul/constants.h
@@ -0,0 +1,14 @@
+! Very slow debug mode or not: open many ASCII files from each node to print 
+! debug information...
+! ALWAYS set this flag to false and recompile the whole code before starting
+! big production runs
+!
+!
+  logical, parameter :: SLOW_DEBUG_MODE = .false. !!! . true.
+!
+!
+!  timer
+  logical, parameter :: USE_TIMER = .true.
+!
+
+
