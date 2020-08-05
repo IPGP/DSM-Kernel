5af23d8df2e7d3d6f24d534afe9ff8432b369069
Nobuaki Fuji <seismobassoon@gmail.com>
2020-08-05 20:17:56 +0200

    writeManyVTK

--- examples/ak135/writeManyVTK.py
+++ examples/ak135/writeManyVTK.py
@@ -19,6 +19,42 @@ def read_fortran_record(binfile, count, dtype):
 #fname_kernel='output/test90.TP.S.T.200s20s.kernel'
 #fname_grid='output/test90.TP.S.T.beta.grid'
 
+def test(fA,fB):
+    print(fA,fB)
+def write_vts2(kernel,fname_grid,fname_vts):
+    # read grid info:
+    nr, nphi, ntheta, nktype = read_fortran_record(gridfile, count=4, dtype=np.int32)
+    nktype += 1 # starts counting from zero (should be changed in the code?)
+    radii = read_fortran_record(gridfile, count=nr, dtype=np.float32)
+    phis = read_fortran_record(gridfile, count=nphi * ntheta, dtype=np.float32)
+    thetas = read_fortran_record(gridfile, count=nphi * ntheta, dtype=np.float32)
+    # write vtk file
+    xgrid = np.outer(np.sin(np.radians(thetas)) * np.cos(np.radians(phis)), radii)
+    ygrid = np.outer(np.sin(np.radians(thetas)) * np.sin(np.radians(phis)), radii)
+    zgrid = np.outer(np.cos(np.radians(thetas)), radii)
+
+    xgrid = xgrid.reshape(ntheta, nphi, nr)
+    ygrid = ygrid.reshape(ntheta, nphi, nr)
+    zgrid = zgrid.reshape(ntheta, nphi, nr)
+
+    point_data = {'s'+'{:d}'.format(name): data for name, data in zip(range(nktype), kernel)}
+
+    gridToVTK(fname_vts, xgrid, ygrid, zgrid, pointData=point_data)
+
+    phis = phis.reshape(ntheta, nphi)
+    thetas = phis.reshape(ntheta, nphi)
+
+    # read kernel data:
+
+    # write some output information
+    r_min, r_max = radii[0], radii[-1]
+    phi_min, phi_max = phis[0, 0], phis[0, -1]
+    theta_min, theta_max = thetas[0, 0], thetas[-1, 0]
+    print ('kernel dimensions (nr={}, nphi={}, ntheta={})'.format(nr, nphi, ntheta))
+    print ('kernel types: {}'.format(nktype))
+    print ('radius range = {} -> {}'.format(r_min, r_max))
+    print ('phis range = {} -> {}'.format(phi_min, phi_max))
+    print ('thetas range = {} -> {}'.format(theta_min, theta_max))
 
 def write_vts(fname_kernel,fname_grid,fname_vts):
 #fname_kernel='output/test90.TP.SS.T.200s20s.kernel'
@@ -50,7 +86,7 @@ def write_vts(fname_kernel,fname_grid,fname_vts):
     ygrid = ygrid.reshape(ntheta, nphi, nr)
     zgrid = zgrid.reshape(ntheta, nphi, nr)
 
-    point_data = {'{:d}'.format(name): data for name, data in zip(range(nktype), kernel)}
+    point_data = {'s'+'{:d}'.format(name): data for name, data in zip(range(nktype), kernel)}
 
     gridToVTK(fname_vts, xgrid, ygrid, zgrid, pointData=point_data)
 
@@ -68,5 +104,21 @@ def write_vts(fname_kernel,fname_grid,fname_vts):
     print ('radius range = {} -> {}'.format(r_min, r_max))
     print ('phis range = {} -> {}'.format(phi_min, phi_max))
     print ('thetas range = {} -> {}'.format(theta_min, theta_max))
+    return kernel
 
-
+f0='/home/nobuaki/scratch/Earth/ak135/kernel/'
+distance=np.arange(50,100,10).tolist()
+#print(distance)
+for idistance in distance:
+    Skernel=f0+'test'+'{:d}'.format(idistance)+'.TP.S.T.100s20s.kernel'
+    Sgrid=f0+'test'+'{:d}'.format(idistance)+'.TP.S.T.beta.grid'
+    Svtk=f0+'{:d}'.format(idistance)+'.S.vts'
+    SSkernel=f0+'test'+'{:d}'.format(idistance)+'.TP.SS.T.100s20s.kernel' 
+    SSgrid=f0+'test'+'{:d}'.format(idistance)+'.TP.SS.T.beta.grid'   
+    SSvtk=f0+'{:d}'.format(idistance)+'.SS.vts'
+    diffvtk=f0+'{:d}'.format(idistance)+'.diff.vts'
+    #print(diffvtk)
+    kernelS=write_vts(Skernel,Sgrid,Svtk)
+    kernelSS=write_vts(SSkernel,SSgrid,SSvtk)
+    kernelD=kernelSS-kernelS
+    write_vts2(kernelD,Sgrid,diffvtk) 
