8ed42736bf6daa0cd59e72603ed0475e5d12e5b7
Nobuaki Fuji <nobuaki@ipgp.fr>
2021-09-02 15:07:16 +0200

    thanks ssuting

--- src/KernelMaker/KernelMaker-1.0.0.f90
+++ src/KernelMaker/KernelMaker-1.0.0.f90
@@ -543,7 +543,7 @@ program KernelMaker
   tsgtF=cmplx(0.d0)
   rsgtF=cmplx(0.d0)
   synnF=cmplx(0.d0)
-
+   print *, "coucou  synn=0"
   synnomega=cmplx(0.d0)
   tsgtomega=cmplx(0.d0)
   rsgtomega=cmplx(0.d0)
